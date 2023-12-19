0f8529c303e07c396d8d4adb8644835c2f8e52f7
SherylAnnNavz <sherylnavarro29@gmail.com>
2023-12-19 17:02:41 +0800

    Add some changes

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const apiKey = '1bfdbff05c2698dc917dd28c08d41096';
 const baseUrl = 'https://api.themoviedb.org/3';
 
+document.addEventListener('DOMContentLoaded', () => {
+    // Initial load of upcoming movies
+    getUpcomingMovies();
+
+    // Add event listener for search input
+    const searchInput = document.getElementById('searchInput');
+    searchInput.addEventListener('input', function () {
+        const query = searchInput.value.trim();
+        if (query !== '') {
+            searchMovie(query);
+        } else {
+            // If the search input is empty, reload upcoming movies
+            getUpcomingMovies();
+        }
+    });
+
+    // Check if the URL contains a movie ID (for direct linking to details page)
+    const urlParams = new URLSearchParams(window.location.search);
+    const movieIdFromUrl = urlParams.get('movieId');
+
+    if (movieIdFromUrl) {
+        showMovieDetails(movieIdFromUrl);
+    }
+});
+
 // Function to fetch upcoming movies
 async function getUpcomingMovies() {
     try {
@@ -61,39 +86,33 @@ async function searchMovie(query) {
 async function showMovieDetails(movieId) {
     try {
         const response = await fetch(`${baseUrl}/movie/${movieId}?api_key=${apiKey}&language=en-US`);
-        if (!response.ok) {
-            throw new Error(`Error fetching movie details: ${response.statusText}`);
-        }
+        console.log('API Response:', response);
 
-        const movieDetails = await response.json();
-        displayMovieDetails(movieDetails);
+        if (response.ok) {
+            const movieDetails = await response.json();
+            console.log('Movie Details:', movieDetails);
+            displayMovieDetails(movieDetails);
+            // Fetch and display similar movies
+            getSimilarMovies(movieId);
+        } else {
+            console.error('Error fetching movie details:', response.statusText);
+        }
     } catch (error) {
         console.error('Error fetching movie details:', error);
     }
 }
 
-
 // Function to display movie details
 function displayMovieDetails(movieDetails) {
-    console.log('Movie Details:', movieDetails);
+    console.log('Displaying Movie Details:', movieDetails);
 
     const movieDetailsContainer = document.getElementById('movieDetails');
     movieDetailsContainer.innerHTML = `
-        <img src="http://image.tmdb.org/t/p/w500/${movieDetails.poster_path}" alt="${movieDetails.title}">
         <h1>${movieDetails.title}</h1>
         <p>${movieDetails.overview}</p>
-        <!-- Add more details as needed -->
-
-        <h2>Similar Movies</h2>
-        <ul id="similarMoviesList"></ul>
-
-        <button onclick="goBack()">Go Back</button>
+        <img src="http://image.tmdb.org/t/p/w500/${movieDetails.poster_path}" alt="${movieDetails.title}">
     `;
-
-    // Fetch and display similar movies
-    getSimilarMovies(movieDetails.id);
 }
-
 
 // Function to fetch and display similar movies
 async function getSimilarMovies(movieId) {
@@ -109,6 +128,7 @@ async function getSimilarMovies(movieId) {
 // Function to display similar movies
 function displaySimilarMovies(similarMovies) {
     const similarMoviesList = document.getElementById('similarMoviesList');
+    similarMoviesList.innerHTML = '<h2>Similar Movies</h2>';
 
     similarMovies.forEach(movie => {
         const listItem = document.createElement('li');
@@ -120,27 +140,4 @@ function displaySimilarMovies(similarMovies) {
 // Function to go back to the index page
 function goBack() {
     window.location.href = 'index.html';
-}
-
-// Initial load of upcoming movies
-getUpcomingMovies();
-
-// Add event listener for search input
-const searchInput = document.getElementById('searchInput');
-searchInput.addEventListener('input', function () {
-    const query = searchInput.value.trim();
-    if (query !== '') {
-        searchMovie(query);
-    } else {
-        // If the search input is empty, reload upcoming movies
-        getUpcomingMovies();
-    }
-});
-
-// Check if the URL contains a movie ID (for direct linking to details page)
-const urlParams = new URLSearchParams(window.location.search);
-const movieIdFromUrl = urlParams.get('movieId');
-
-if (movieIdFromUrl) {
-    showMovieDetails(movieIdFromUrl);
 }
